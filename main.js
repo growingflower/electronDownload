8d7464a6255e445a6e074c98567608eafc5c20ff
growingflower <wqwoshiwq@163.com>
2018-06-20 18:20:09 +0800

    暂时上传

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow,ipcMain,shell} = require('electron');
+const {app, BrowserWindow,ipcMain,shell,dialog} = require('electron');
 const path = require('path');
 const glob = require('glob');
 const LokiDB = require('lokijs');
@@ -48,7 +48,9 @@ class DemoDownload {
     initReceiveInfo(win){
       ipcMain.on('startdownload',(event, arg) => {
         try{
-          win.webContents.downloadURL("http://dldir1.qq.com/qqfile/QQforMac/QQ_V6.4.0.dmg")
+          // "https://dldir1.qq.com/qqfile/QQIntl/QQi_PC/QQIntl2.11.exe"
+          // "http://dldir1.qq.com/qqfile/QQforMac/QQ_V6.4.0.dmg"  testurl
+          win.webContents.downloadURL("https://docs.qq.com/?adtag=QQxiazaiyeanniu#id-home-download")
         }catch(err){
           console.log(err)
         }
@@ -56,50 +58,51 @@ class DemoDownload {
     }
 
     listenToDownload(win){
-      try{
         win.webContents.session.on('will-download', (event, item, webContents) => {
-          try{
+            if(null === item || null ===webContents){
+              event.preventDefault()
+              let opts = {type:'warning',message:"wrong downloadUrl"}
+              dialog.showMessageBox(win,opts)
+              return
+            }
             let filesize = item.getTotalBytes();
-            let startTime = item.getStartTime().toString();
+            let startTime = item.getStartTime();
             let filename = item.getFilename();
             let fileUrl = item.getURL();
+            let receivedBytes = item.getReceivedBytes();
+            let getETag = item.getETag();
+            
             let flieInfos = [];
             flieInfos.push(filesize,startTime,filename,fileUrl);
             let flieString = flieInfos.join("+");
             //savedownloaditems
-            itemsCollection.insert({name:filename,startTime:startTime, downloaditem: item,webContents:webContents});
-            let downloaditem = itemsCollection.find({'startTime':startTime})[0].downloaditem;
-            // item.setSavePath('/Users/mingdao/Downloads/'+filename);
-            item.setSavePath('/Users/wuqian/Downloads/'+filename);
+            let itembeginning = itemsCollection.insert({itemid:startTime.toString(),downloaditem:item})
+            // itemsCollection.insert({name:filename,startTime:startTime, downloaditem: item,webContents:webContents});
+            // let downloaditem = itemsCollection.find({'startTime':startTime})[0].downloaditem;
+            item.setSavePath('/Users/mingdao/Downloads/'+filename);
+            // item.setSavePath('/Users/wuqian/Downloads/'+filename);
             webContents.send('downloading',flieString)
-            ipcMain.on('downloadingInfo',(event,arg) => {
-              let fileInfos = arg.split("+");
-              let [filesize,startTimeBack,downloadedSize,filename,fileUrl] = fileInfos
-              let downloaditeminfo = itemsCollection.find({'startTime':startTimeBack})
-              let webContents = itemsCollection.find({'startTime':startTimeBack})[0].webContents
-              this.listenToOneItem(downloaditeminfo,webContents)
-            })
-          }catch(err){
-            console.log(err)
-          }
-         
+            // ipcMain.on('downloadingInfo',(event,arg) => {
+            //   let fileInfos = arg.split("+");
+            //   let [filesize,startTimeBack,downloadedSize,filename,fileUrl] = fileInfos
+            //   let downloaditeminfo = itemsCollection.find({'startTime':startTimeBack})
+            //   let webContents = itemsCollection.find({'startTime':startTimeBack})[0].webContents
+            //   this.listenToOneItem(downloaditeminfo,webContents,win)
+            // })
+            this.listenToOneItem(item,webContents,itembeginning,win)
         })
-      }catch(err){
-        console.log(err)
-      }
     }
 
-    listenToOneItem(downloaditeminfo,webContents){
-      let downloaditem = downloaditeminfo[0].downloaditem;
+    listenToOneItem(downloaditem,webContents,itembeginning,win){
+      // let downloaditem = downloaditeminfo[0].downloaditem;
       // let fileurl = '/Users/mingdao/Downloads/' + downloaditeminfo[0].name;
-      let fileurl = app.getPath('downloads') + downloaditeminfo[0].name;
+      let fileurl = app.getPath('downloads')+'/' + downloaditem.getFilename();
+      console.log(fileurl,"mainfileurl")
       ipcMain.on('cancelDownload',(event,arg)=>{
         downloaditem.cancel()
       });
 
       ipcMain.on('continueDownload',(event,arg)=>{
-        console.log(222) 
-        console.log(downloaditem.isDestroyed(),'22222')
         downloaditem.resume()
       });
 
@@ -112,6 +115,7 @@ class DemoDownload {
           console.log('Download successfully')
           webContents.send('completed',state)
           ipcMain.on('openDir',(event,arg)=>{
+            console.log("mianopenDir")
             shell.showItemInFolder(fileurl)
           })
         }else if(state === 'cancelled'){
@@ -124,6 +128,11 @@ class DemoDownload {
       });
 
       downloaditem.on('updated',(event, state)=>{
+        if(downloaditem.isDestroyed()){
+          event.preventDefault()
+          let opts = {type:'warning',message:" download failed"}
+          dialog.showMessageBox(win,opts)
+        }
         if (state === 'interrupted') {
           console.log('Download is interrupted but can be resumed')
         } else if (state === 'progressing') {
@@ -131,9 +140,17 @@ class DemoDownload {
             console.log('isPaused')
             webContents.send('isPaused')
           } else {
+            console.log(`LastModifiedTime: ${downloaditem.getLastModifiedTime()}`)
+            console.log(`getTag: ${downloaditem.getETag()}`)
             console.log(`Received bytes: ${downloaditem.getReceivedBytes()}`)
-            let receivedBytes = downloaditem.getReceivedBytes()
-            webContents.send('receivedBytes',receivedBytes)
+            itembeginning.downloaditem = downloaditem;
+            let receivedBytes = downloaditem.getReceivedBytes();
+            let startTime = downloaditem.getStartTime();
+            let completedbytes = 0
+            completedbytes += receivedBytes;
+            let speed = completedbytes/(Number(new Date().getTime()/1000) - Number(startTime))
+            itemsCollection.update(itembeginning)
+            webContents.send('receivedBytes',receivedBytes,speed,completedbytes)
           }
         }
       }) ;
--- renderProcess/download/download.js
+++ renderProcess/download/download.js
@@ -11,7 +11,7 @@ class DownloadBlock {
         let downloadBtn = document.getElementById('downloadButton')
         let downloadUrlinput = document.getElementById('downloadUrl')
         downloadBtn.addEventListener('click',(event)=>{
-            let url = downloadUrlinput.value;
+            let url = $.trim(downloadUrlinput.value);
             if(url){
                 ipcRenderer.send('startdownload',url)
                 this.listenDownloading();
@@ -23,32 +23,31 @@ class DownloadBlock {
 
     listenDownloading(){
         ipcRenderer.on("downloading",(event, arg)=>{
-            ipcRenderer.send('downloadingInfo',arg)
+            // ipcRenderer.send('downloadingInfo',arg)
             let fileInfos = arg.split("+")
             let [filesize,startTime,filename,fileUrl] = fileInfos
             let fileValue = {
                 'name': filename,
                 'url': fileUrl,
                 'status': 1,
-                'downloaded': 1000000,
+                'downloaded': 0,
                 'total': filesize,
-                'speed': 1000,
+                'speed': 0,
                 'id': Array.apply(null, new Array(5)).
                     map(a => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)).
                     join('-')
                 };
             addFile(fileValue)
-            ipcRenderer.on('receivedBytes',(event,arg)=>{
+            ipcRenderer.on('receivedBytes',(event,arg,speed,completedbytes)=>{
                 fileValue.downloaded = Number(arg);
                 updateFile(fileValue.id, Object.assign({}, fileValue, {
+                        downloaded:completedbytes,
+                        speed:speed,
                         status: fileValue.downloaded === fileValue.total ? STATUS.completed : STATUS.progressing
                       }));
-                let cancelBtn = document.getElementsByClassName('cancel');
-                let continueBtn = document.getElementsByClassName('continue');
-                let pauseBtn = document.getElementsByClassName('pause');
-                this.addClassEvent(cancelBtn,'cancelDownload',fileValue);   
-                this.addClassEvent(continueBtn,'continueDownload',fileValue);   
-                this.addClassEvent(pauseBtn,'pauseDownload',fileValue);   
+                this.addClassEvent('cancelDownload',fileValue);   
+                this.addClassEvent('continueDownload',fileValue);   
+                this.addClassEvent('pauseDownload',fileValue);   
                 
             })
             ipcRenderer.on('isPaused',(event,arg)=>{
@@ -56,51 +55,11 @@ class DownloadBlock {
                 //     status:STATUS.paused
                 //   }));
             })
-            
-            // let data = [
-            //       {
-            //         name: filename,
-            //         url: fileUrl,
-            //         status: 1,
-            //         downloaded: 0,
-            //         total: filesize,
-            //         speed: 1000
-            //       }].map(function (v) {
-            //             return Object.assign({}, v, {
-            //             id: Array.apply(null, new Array(5)).
-            //                 map(a => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)).
-            //                 join('-')
-            //             });
-            //         });
-            // render(data);
-            // let cancelBtn = document.getElementsByClassName('cancel');
-            // let continueBtn = document.getElementsByClassName('continue');
-            // let pauseBtn = document.getElementsByClassName('pause');
-            // this.addClassEvent(cancelBtn,'cancelDownload',fileValue);   
-            // this.addClassEvent(continueBtn,'continueDownload',fileValue);   
-            // this.addClassEvent(pauseBtn,'pauseDownload',fileValue);   
             ipcRenderer.on('completed',(event,arg)=>{
                 updateFile(fileValue.id, Object.assign({}, fileValue, {
                         status:  STATUS.completed
                       }));                
-                // let data = [
-                //     {
-                //       name: filename,
-                //       url: fileUrl,
-                //       status: 3,
-                //       downloaded: downloadedSize,
-                //       total: filesize,
-                //       speed: 1000
-                //     }].map(function (v) {
-                //           return Object.assign({}, v, {
-                //           id: Array.apply(null, new Array(5)).
-                //               map(a => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)).
-                //               join('-')
-                //           });
-                //       });
-            //   render(data)  
-              let popenDirBtn = document.getElementsByClassName('openDir');
-              this.addClassEvent(popenDirBtn,'openDir')
+              this.addClassEvent('openDir',fileValue)
             })   
             ipcRenderer.on('interrupted',(event,arg)=>{
                 updateFile(fileValue.id, Object.assign({}, fileValue, {
@@ -108,59 +67,100 @@ class DownloadBlock {
                   }));      
             }) 
             let retryBtn = document.getElementsByClassName('retry');
-            this.addClassEvent(retryBtn,'retryDownload',fileValue);  
+            this.addClassEvent('retryDownload',fileValue);  
            
         })
         
     }
 
-    addClassEvent(elements,type,fileValue){
+    addClassEvent(type,fileValue){
+        let id = fileValue.id;
         switch(type){
             case "openDir":
-                $('.body .con').on('click', '.fileItem .openDir', function (e) {
-                    var name = $('.openDir').parents('.fileItem').find('.data').data('item').name;
+                $("#"+id).on('click','.openDir',function(e){
+                    console.log('openDir')
+                    let value = $('.openDir').parents('.fileItem').find('.data').data('item').name;
                     ipcRenderer.send('openDir');
-                });
+                })
+                // $('.body .con').on('click', '.fileItem .openDir', function (e) {
+                //     var name = $('.openDir').parents('.fileItem').find('.data').data('item').name;
+                //     ipcRenderer.send('openDir');
+                // });
                 break;
             case "cancelDownload":
-                $('.body .con').on('click', '.fileItem .cancel', function (item) {
-                    var value = $('.cancel').parents('.fileItem').find('.data').data('item');
-                    updateFile(value.id, Object.assign({}, value, {
-                    status: STATUS.cancaled
+                $("#"+id).on('click','.cancel',function(e){
+                    console.log('cancel')
+                    let value = $('.cancel').parents('.fileItem').find('.data').data('item');
+                    updateFile(id, Object.assign({}, value, {
+                            status: STATUS.cancel
                     }));
                     ipcRenderer.send('cancelDownload');
-                    console.log('取消 id.', value.id, ' ' + value.name);
-                });
+                })
+                // $('.body .con').on('click', '.fileItem .cancel', function (item) {
+                //     var value = $('.cancel').parents('.fileItem').find('.data').data('item');
+                //     updateFile(value.id, Object.assign({}, value, {
+                //     status: STATUS.cancaled
+                //     }));
+                //     ipcRenderer.send('cancelDownload');
+                //     console.log('取消 id.', value.id, ' ' + value.name);
+                // });
           
                 break;
             case "continueDownload":
-                $('.body .con').on('click', '.fileItem .continue', function (e) {
-                    var value = $('.continue').parents('.fileItem').find('.data').data('item');
-                    updateFile(value.id, Object.assign({}, value, {
-                    status: STATUS.progressing
+                $("#"+id).on('click','.continue',function(e){
+                    console.log('continue')
+                    let value = $('.continue').parents('.fileItem').find('.data').data('item');
+                    updateFile(id, Object.assign({}, value, {
+                            status: STATUS.cancel
                     }));
                     ipcRenderer.send('continueDownload');
-                });
+                })
+                // $('.body .con').on('click', '.fileItem .continue', function (e) {
+                //     var value = $('.continue').parents('.fileItem').find('.data').data('item');
+                //     updateFile(value.id, Object.assign({}, value, {
+                //     status: STATUS.progressing
+                //     }));
+                //     ipcRenderer.send('continueDownload');
+                // });
                 break;
             case "pauseDownload":
-                $('.body .con').on('click', '.fileItem .pause', function (e) {
-                    var value = $('.pause').parents('.fileItem').find('.data').data('item');
-                    updateFile(value.id, Object.assign({}, value, {
-                      status: STATUS.paused
+                $("#"+id).on('click','.pause',function(e){
+                    console.log('pause')
+                    let count = 0;
+                    count++
+                    console.log(count)
+                    let value = $('.pause').parents('.fileItem').find('.data').data('item');
+                    updateFile(id, Object.assign({}, value, {
+                                status: STATUS.paused
                     }));
-                    ipcRenderer.send('pauseDownload',value.url);
-                });
+                    ipcRenderer.send('pauseDownload');
+                })
+                // $('.body .con').on('click', '.fileItem .pause', function (e) {
+                //     var value = $('.pause').parents('.fileItem').find('.data').data('item');
+                //     updateFile(value.id, Object.assign({}, value, {
+                //       status: STATUS.paused
+                //     }));
+                //     ipcRenderer.send('pauseDownload',value.url);
+                // });
                 break;
             case "retryDownload":
-                console.log(2222)
-                $('.body .con').on('click', '.fileItem .retry', function (e) {
-                    var value = $('.retry').parents('.fileItem').find('.data').data('item');
-                    console.log(value,"value")
-                    updateFile(value.id, Object.assign({}, value, {
-                      status: STATUS.progressing
+                $("#"+id).on('click','.retry',function(e){
+                    console.log('retry')
+                    let value = $('.retry').parents('.fileItem').find('.data').data('item');
+                    updateFile(id, Object.assign({}, value, {
+                            status: STATUS.paused
                     }));
                     ipcRenderer.send('startdownload',url)
-                });
+                })
+
+                // $('.body .con').on('click', '.fileItem .retry', function (e) {
+                //     var value = $('.retry').parents('.fileItem').find('.data').data('item');
+                //     console.log(value,"value")
+                //     updateFile(value.id, Object.assign({}, value, {
+                //       status: STATUS.progressing
+                //     }));
+                //     ipcRenderer.send('startdownload',url)
+                // });
                 break;
             default:
                 console.log('err')
